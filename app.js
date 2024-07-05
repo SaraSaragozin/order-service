3f28a5c2b3f120ab7444323c07f0809a7d7a33e9
SaraSaragozin <saralizeth0711@gmail.com>
2024-07-05 12:20:44 -0500

    Create the project and add app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const orderRoutes = require('./routes/orderRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use('/orders', orderRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Order service listening at http://localhost:${PORT}`);
+});
